c585d793daf706c0eaa2b0bc8985c7b5bd918137
luuk kessels <lmpkessels@gmail.com>
2025-06-26 13:13:00 +0200

    add: full adder to receive a total and overflow

---
+++ src/logicflow/full_adder.rs
@@ -0,0 +1,35 @@
+mod gates;
+use gates::*;
+
+// Full adder returning Sum and Carry out in boolean data-type.
+fn full_addr(a: &bool, b: &bool, cin: &bool) -> (bool, bool) {
+    // Sum logic -> a AND b = bit, then Bit AND Cin, is end Bit.
+    let sum = xor(&xor(a, b), cin);
+    // Cout logic -> (a AND b = Bit), OR (a XOR b = Bit, AND Cin = Bit) = Bit.
+    let cout = or(and(*a, *b), and(*cin, xor(a, b)));
+
+    // Return both values as owner.
+    (sum, cout)
+}
+
+// eight bit full adder, receiving as argument two arrays, and boolean carry in.
+// Fn returns a array with 8 boolean expressions + overflow.
+fn eight_bit_full_addr(a: &[bool; 8], b: &[bool; 8]) -> ([bool; 8], bool) {
+    // Array, default boolean expression is false, range is 8 boolean expressions.
+    let mut total = [false; 8];
+    // Carry out, by default false.
+    let mut cout = false;
+
+    // Get i out of range 0..8, then reverse because we start at righ-most lsb.
+    for i in (0..8).rev() {
+        // Function creating a total then assigning it to sum and cin.
+        let (addr, overflow) = full_addr(&a[i], &b[i], &cout);
+        // Implement addr in total[i].
+        total[i] = addr;
+        // Carry out is overflow.
+        cout = overflow;
+    }
+
+    // Return array and carry out(overflow in this case).
+    (total, cout)
+}
