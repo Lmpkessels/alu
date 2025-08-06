0cc27a2e86aedea16abf06ad6e07185ed5cddfe7
luuk kessels <lmpkessels@gmail.com>
2025-08-06 10:55:27 +0200

    update: changed data types due to using to large data types for the range am working with

--- src/operators/decimal_to_binary.rs
+++ src/operators/decimal_to_binary.rs
@@ -1,4 +1,6 @@
-fn decimal_to_binary(mut decimal_num: u32) -> [u32; 8] {
+// Takes a decimal number as argument and returns and array of bits with the
+// value of the decimal argument.
+fn decimal_to_binary(mut decimal_num: u8) -> [u8; 8] {
     let mut result = [0; 8];
     
     for i in (0..8).rev() {
