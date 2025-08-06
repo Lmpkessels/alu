75c4cc4170432504d3897c92a506ca6c9162a007
luuk kessels <lmpkessels@gmail.com>
2025-08-06 11:01:36 +0200

    docs: update comments for difference and borrow out

--- src/operators/subtraction.rs
+++ src/operators/subtraction.rs
@@ -1,11 +1,13 @@
 use crate::gates::basic::{not, and, or, xor};
 
-// 
+// Returns difference after applying bit_a XOR bit_b = bit XOR borrow_out = result. 
+// So it knows what to write down at the bits column.
 pub fn difference_bit(bit_a: u8, bit_b: u8, borrow_in_bit: u8) -> u8 {
     xor(xor(bit_a, bit_b), borrow_in_bit)
 }
 
-// 
+// Check if a borrow if needed so it never ends up in underflow when using unassigned
+// math.
 pub fn borrow_out_bit(bit_a: u8, bit_b: u8, borrow_in_bit: u8) -> u8 {
     or(and(not(bit_a), bit_b), and(not(xor(bit_a, bit_b)), borrow_in_bit))
 }
