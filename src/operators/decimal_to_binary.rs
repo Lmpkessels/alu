c7511179eef8d91c9a4b40a925d69ef459169053
luuk kessels <lmpkessels@gmail.com>
2025-08-06 12:42:55 +0200

    feat: add binary to decimal function with test

--- src/operators/decimal_to_binary.rs
+++ src/operators/decimal_to_binary.rs
@@ -11,6 +11,19 @@ fn decimal_to_binary(mut decimal_num: u8) -> [u8; 8] {
     result
 }
 
+// Takes a binary array starts a MSB, index 0, and returns its decimal value.
+// Each bit is multiplied by 2^(7 - index) using a left shift, subtracting the index
+// is used to decrease the shift as you go left.
+fn binary_to_decimal(bits: [u8; 8]) -> u8 {
+    let mut result = 0;
+
+    for i in 0..8 {
+        result += bits[i] << (7 - i);
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -20,6 +33,15 @@ mod test {
         let decimal = 34;
         let result = decimal_to_binary(decimal);
         let expected = [0, 0, 1, 0, 0, 0, 1, 0];
+
+        assert_eq!((result), (expected));
+    }
+
+    #[test]
+    fn goes_from_array_with_byte_to_returning_its_decimal() {
+        let byte = [1, 0, 1, 1, 0, 1, 0, 1];
+        let result = binary_to_decimal(byte);
+        let expected = 181;
 
         assert_eq!((result), (expected));
     }
