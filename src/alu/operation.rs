2889999da01a80defb54603b77987071296cfba1
luuk kessels <lmpkessels@gmail.com>
2025-08-16 16:19:13 +0200

    add: integer division logic as feature to ALU

--- src/alu/operation.rs
+++ src/alu/operation.rs
@@ -1,12 +1,15 @@
 use crate::operators::transformer::{
     decimal_to_binary, binary_to_decimal, addition_decimal_to_binary
 };
-use crate::adders::{addition_32_bit, subtraction_32_bit, multiply_16_x_16};
+use crate::adders::{
+    addition_32_bit, subtraction_32_bit, multiply_16_x_16, division_32
+};
 
 enum Operation {
     Add,
     Subtract,
     Multiply,
+    Devide,
 }
 
 fn alu(a: u32, b: u32, gate: Operation) -> u32 {
@@ -19,15 +22,18 @@ fn alu(a: u32, b: u32, gate: Operation) -> u32 {
     let addition = addition_32_bit(word_a, word_b);
     let subtraction = subtraction_32_bit(word_a, word_b);
     let multiplication = multiply_16_x_16(multiply_word_a, multiply_word_b);
+    let division = division_32(word_a, word_b);
 
     let dec_addition = binary_to_decimal(addition);
     let dec_subtraction = binary_to_decimal(subtraction);
     let dec_multiplication = binary_to_decimal(multiplication);
+    let dec_division = binary_to_decimal(division);
 
     match gate {
         Operation::Add => dec_addition,
         Operation::Subtract => dec_subtraction,
-        Operation::Multiply => dec_multiplication
+        Operation::Multiply => dec_multiplication,
+        Operation::Devide => dec_division,
     }
 }
 
@@ -64,6 +70,17 @@ mod test {
         let operator = Operation::Multiply;
         let result = alu(a, b, operator);
         let expected = 759;
+
+        assert_eq!((result), (expected));
+    }
+
+    #[test]
+    fn devides_a_by_b() {
+        let a = 28;
+        let b = 11;
+        let operator = Operation::Devide;
+        let result = alu(a, b, operator);
+        let expected = 2;
 
         assert_eq!((result), (expected));
     }
